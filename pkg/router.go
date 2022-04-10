e6e38f103c2130ebffefdd32f220138a1c86bf57
daichi1123 <badass.haruzion@gmail.com>
2022-04-10 23:04:35 +0900

    feat: get port number from config file

--- pkg/router.go
+++ pkg/router.go
@@ -2,16 +2,16 @@ package pkg
 
 import (
 	"fmt"
+	"go_api/configs"
 	"net/http"
 )
 
 func Router() error {
-	// server()
+	fmt.Println(configs.Config.Port)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Print("テスト用")
 	})
-	http.ListenAndServe(":8080", mux)
-	// http.ListenAndServe(Config.Port, mux)
+	http.ListenAndServe(configs.Config.Port, mux)
 	return nil
 }
