8ed6f3c22e52bb28b7183e6431b23f7ce07c4d2f
daichi1123 <badass.haruzion@gmail.com>
2022-04-12 00:24:30 +0900

    WIP: add header.go

---
+++ utils/header.go
@@ -0,0 +1,11 @@
+package utils
+
+import "net/http"
+
+func HeaderInfo(w http.ResponseWriter, r http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	w.Header().Add("Content-Type", "application/json")
+}
