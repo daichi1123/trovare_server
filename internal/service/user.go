22b78b0c9db6b0705a3416e0a4ae232ce7811747
daichi1123 <badass.haruzion@gmail.com>
2022-04-13 01:38:16 +0900

    feat: moved Sesstion struct

--- internal/service/user.go
+++ internal/service/user.go
@@ -10,11 +10,3 @@ type User struct {
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 }
-
-type Session struct {
-	ID        int
-	UUID      string
-	Email     string
-	UserID    int
-	CreatedAt time.Time
-}
