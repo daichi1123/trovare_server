9ae27421d7861c558e74808445c4e875ee2c7bb2
daichi1123 <badass.haruzion@gmail.com>
2022-04-12 09:26:04 +0900

    feat: add handleFunc

--- pkg/router.go
+++ pkg/router.go
@@ -4,12 +4,14 @@ import (
 	"go_api/cmd/pkg"
 	"go_api/configs"
 	"go_api/internal/userHandler"
+	"go_api/utils"
 	"net/http"
 )
 
 func Router() error {
 	userHandler.User.CreateUser(userHandler.User{})
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", utils.RedirectIndex)
 	mux.HandleFunc("/index", pkg.Index)
 	http.ListenAndServe(configs.Config.Port, mux)
 	return nil
