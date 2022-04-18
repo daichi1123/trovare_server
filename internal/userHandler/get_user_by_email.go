ecf463f95eec51d89aedefeff9b21c54b907ba95
daichi1123 <badass.haruzion@gmail.com>
2022-04-18 23:53:41 +0900

    feat: check uuid

--- internal/userHandler/get_user_by_email.go
+++ internal/userHandler/get_user_by_email.go
@@ -1,6 +1,7 @@
 package userHandler
 
 import (
+	"fmt"
 	"go_api/utils"
 	"log"
 )
@@ -8,6 +9,26 @@ import (
 func GetUserByEmail(email string) (user User, err error) {
 	user = User{}
 	const getUserByEmail = `SELECT id, uuid, name, email, password, created_at FROM users WHERE email = ?;`
+
+	utils.OpenDb()
+	err = utils.Db.QueryRow(getUserByEmail, email).Scan(&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt)
+	if err != nil {
+		//TODO: このエラーの際frontのbodyにエラー分を返却したい
+		log.Fatalln(err)
+	}
+	defer utils.Db.Close()
+
+	return
+}
+func GetUserByEmailForSession(email string) (user User, err error) {
+	user = User{}
+	const getUserByEmail = `SELECT id, uuid, name, email, password, created_at FROM users WHERE email = ?;`
+	fmt.Print(email)
 
 	utils.OpenDb()
 	err = utils.Db.QueryRow(getUserByEmail, email).Scan(&user.ID,
