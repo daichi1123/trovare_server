ebfe56c8b03f32387bbb516a0692bd1f6d69d719
daichi1123 <badass.haruzion@gmail.com>
2022-04-29 19:46:23 +0900

    feat: make tables func

--- internal/service/base.go
+++ internal/service/base.go
@@ -9,10 +9,15 @@ import (
 )
 
 const (
-	tableNameUser       = "users"
-	tableNameSession    = "sessions"
-	tableNameRestaurant = "restaurants"
+	tableNameUser            = "users"
+	tableNameSession         = "sessions"
+	tableNameRestaurant      = "restaurants"
+	tableNameFood            = "foods"
+	tableNameDrink           = "drinks"
+	tableNameRestaurantGenre = "restaurant_genres"
 )
+
+// MEMO: マイグレーションファイルも1ファイル毎に分けているのでこのファイルも分けるべきではないか??
 
 var Db *sql.DB
 var err error
@@ -24,32 +29,32 @@ func MakeTable() {
 	}
 
 	// users table
-	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	userT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 		id INTEGER PRIMARY KEY AUTO_INCREMENT,
 		uuid VARCHAR(100) NOT NULL UNIQUE,
 		name VARCHAR(100),
 		email VARCHAR(100) NOT NULL UNIQUE,
 		password VARCHAR(100),
 		created_at DATETIME)`, tableNameUser)
-	_, err := Db.Exec(cmdU)
+	_, err := Db.Exec(userT)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	// sessions table
-	cmdS := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	sessionT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 		id INTEGER PRIMARY KEY AUTO_INCREMENT,
 		uuid VARCHAR(100) NOT NULL UNIQUE,
 		email VARCHAR(100),
 		user_id VARCHAR(100),
 		created_at DATETIME)`, tableNameSession)
-	_, err = Db.Exec(cmdS)
+	_, err = Db.Exec(sessionT)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	// restaurants table
-	cmdR := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	restaurantT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
 		id INTEGER PRIMARY KEY AUTO_INCREMENT,
 		name VARCHAR(100),
 		description VARCHAR(1000),
@@ -57,7 +62,42 @@ func MakeTable() {
 		owner_id INTEGER,
 		rating INTEGER,
 		created_at DATETIME)`, tableNameRestaurant)
-	_, err = Db.Exec(cmdR)
+	_, err = Db.Exec(restaurantT)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	foodT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(100),
+		description VARCHAR(1000),
+		restaurant_id INTEGER,
+		created_at DATETIME,
+		updated_at DATETIME)`, tableNameFood)
+	_, err = Db.Exec(foodT)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	drinkT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(100),
+		description VARCHAR(1000),
+		restaurant_id INTEGER,
+		created_at DATETIME,
+		updated_at DATETIME)`, tableNameDrink)
+	_, err = Db.Exec(drinkT)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	RestaurantGenreT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTO_INCREMENT,
+		restaurant_id INTEGER,
+		genre_id INTEGER,
+		created_at DATETIME,
+		)`, tableNameRestaurantGenre)
+	_, err = Db.Exec(RestaurantGenreT)
 	if err != nil {
 		log.Fatalln(err)
 	}
