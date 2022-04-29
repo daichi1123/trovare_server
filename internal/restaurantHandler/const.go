23f1690ebc3526d0ec7a56a112257fb055e7b198
daichi1123 <badass.haruzion@gmail.com>
2022-04-29 14:36:57 +0900

    feat: put together query content in const file

---
+++ internal/restaurantHandler/const.go
@@ -0,0 +1,8 @@
+package restaurantHandler
+
+const (
+	create = `INSERT INTO restaurants (name, description, restaurant_id, owner_id, rating, created_at) values (?, ?, ?, ?, ?, ?)`
+	show   = `SELECT id, name, description, rating, created_at FROM restaurants WHERE id = ?`
+	update = `UPDATE restaurants SET name = ? WHERE id = ?`
+	delete = `DELETE FROM restaurants WHERE id = ?`
+)
