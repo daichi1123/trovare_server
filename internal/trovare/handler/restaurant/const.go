0d36ae2bf338e3ece2ef1e35f97ff5b1ba2bcae4
daichi1123 <badass.haruzion@gmail.com>
2022-05-04 10:08:40 +0900

    feat: change query content

--- internal/trovare/handler/restaurant/const.go
+++ internal/trovare/handler/restaurant/const.go
@@ -2,7 +2,7 @@ package restaurant
 
 const (
 	getAll = `SELECT * FROM restaurants`
-	create = `INSERT INTO restaurants (name, description, restaurant_id, owner_id, rating, created_at) values (?, ?, ?, ?, ?, ?)`
+	create = `INSERT INTO restaurants (name, description, zip_code, address, created_at) values (?, ?, ?, ?, ?)`
 	show   = `SELECT id, name, description, rating, created_at FROM restaurants WHERE id = ?`
 	update = `UPDATE restaurants SET name = ? WHERE id = ?`
 	delete = `DELETE FROM restaurants WHERE id = ?`
