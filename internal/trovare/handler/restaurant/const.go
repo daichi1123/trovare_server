f1ebd886953f25dd90c47acc9bdd9fe198e0ebc7
daichi1123 <badass.haruzion@gmail.com>
2022-05-15 16:13:28 +0900

    add query contents

--- internal/trovare/handler/restaurant/const.go
+++ internal/trovare/handler/restaurant/const.go
@@ -1,9 +1,10 @@
 package restaurant
 
 const (
-	getAll = `SELECT * FROM restaurants`
-	create = `INSERT INTO restaurants (name, description, zip_code, address, created_at) values (?, ?, ?, ?, ?)`
-	show   = `SELECT id, name, description, rating, created_at FROM restaurants WHERE id = ?`
-	update = `UPDATE restaurants SET name = ? WHERE id = ?`
-	delete = `DELETE FROM restaurants WHERE id = ?`
+	getAll   = `SELECT * FROM restaurants`
+	getLists = `SELECT id, name, description, zip_code, address, image_url, rating FROM restaurants`
+	create   = `INSERT INTO restaurants (name, description, rating, zip_code, address, image_url, created_at) values (?, ?, ?, ?, ?, ?, ?)`
+	show     = `SELECT id, name, description, rating, zip_code, address, image_url, created_at FROM restaurants WHERE id = ?`
+	update   = `UPDATE restaurants SET name = ? WHERE id = ?`
+	delete   = `DELETE FROM restaurants WHERE id = ?`
 )
