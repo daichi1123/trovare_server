f71c094b6ded1e1ed4f27d2afb0531b5e0a958ab
daichi1123 <badass.haruzion@gmail.com>
2022-05-22 01:26:06 +0900

    WIP: send restaurant locations

--- internal/trovare/handler/locations/post_current_location.go
+++ internal/trovare/handler/locations/post_current_location.go
@@ -6,12 +6,17 @@ import (
 	"net/http"
 )
 
+// TODO: lat lngをフィルターで半径5キロ分のデータを取得できる様にする
+// TODO: そこで取得できた情報をフロントエンドに送れる様にする
+
 func PostCurrentLocation(w http.ResponseWriter, r *http.Request) {
+	// WIP
 	var currentLocation CurrentLocationReq
 
 	switch r.Method {
 	case http.MethodPost:
 		err := json.NewDecoder(r.Body).Decode(&currentLocation)
+		log.Println(getFiveKmRadiusOfCurrentLocation)
 		if err != nil {
 			w.WriteHeader(401)
 			log.Println(err)
