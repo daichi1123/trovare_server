770ccd787277cf566e45c1b4b6f26837b9d076b3
daichi1123 <badass.haruzion@gmail.com>
2022-05-22 01:24:10 +0900

    feat: add new columns

--- internal/trovare/handler/restaurant/const.go
+++ internal/trovare/handler/restaurant/const.go
@@ -4,7 +4,7 @@ const (
 	getAll         = `SELECT * FROM restaurants`
 	getLists       = `SELECT id, name, description, zip_code, address, image_url, rating FROM restaurants`
 	getRstLocation = `SELECT id, name, zip_code, address, image_url, rating FROM restaurants`
-	create         = `INSERT INTO restaurants (name, description, rating, zip_code, address, image_url, created_at) values (?, ?, ?, ?, ?, ?, ?)`
+	create         = `INSERT INTO restaurants (name, description, rating, zip_code, address, lat, lng, image_url, created_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 	show           = `SELECT id, name, description, rating, zip_code, address, image_url, created_at FROM restaurants WHERE id = ?`
 	update         = `UPDATE restaurants SET name = ? WHERE id = ?`
 	delete         = `DELETE FROM restaurants WHERE id = ?`
