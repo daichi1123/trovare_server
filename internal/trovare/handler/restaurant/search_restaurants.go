64e6c90313a7a32eb5d87be9a3cc2527b23aefd3
daichi1123 <badass.haruzion@gmail.com>
2022-05-28 01:05:20 +0900

    wip: search func by rating and genre

--- internal/trovare/handler/restaurant/search_restaurants.go
+++ internal/trovare/handler/restaurant/search_restaurants.go
@@ -6,69 +6,102 @@ import (
 	"go_api/pkg"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func SearchRestaurants(w http.ResponseWriter, r *http.Request) {
+	// TODO: I have to add codes and query contents
 	switch r.Method {
 	case "GET":
-		if r.URL.Query() {
-
-		}
-
 		restaurant := r.URL.Query().Get("restaurants")
-		log.Println(restaurant)
+		genre := r.URL.Query().Get("genres")
+		rating := r.URL.Query().Get("rating")
 
 		pkg.OpenDb()
 		pkg.Db.Begin()
 
-		rows, err := pkg.Db.Query(getSearchResult, restaurant)
-		defer rows.Close()
-		defer pkg.Db.Close()
+		if restaurant != "" {
+			rows, err := pkg.Db.Query(searchResult, restaurant)
 
-		for rows.Next() {
-			var name string
-			err := rows.Scan(&name)
+			for rows.Next() {
+				var name string
+				err := rows.Scan(&name)
 
-			if err != nil {
-				panic(err.Error())
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				fmt.Println(name)
 			}
-			fmt.Println(name)
-		}
+			defer rows.Close()
 
-		//var rstsInfoRes []CurrentLocationRes
-		//for rows.Next() {
-		//	currentLocationRes := CurrentLocationRes{}
-		//
-		//	//ここの値をcurrentLocationRes.Location.Latみたいにすればいけるのではないか
-		//	err = rows.Scan(
-		//		&currentLocationRes.ID,
-		//		&currentLocationRes.Name,
-		//		&currentLocationRes.Description,
-		//		&currentLocationRes.ZipCode,
-		//		&currentLocationRes.Address,
-		//		&currentLocationRes.Rating,
-		//		&currentLocationRes.Coords.Lat,
-		//		&currentLocationRes.Coords.Lng,
-		//		&distance.Distance)
-		//	rstsInfoRes = append(rstsInfoRes, currentLocationRes)
-		//}
-		//if err != nil {
-		//	w.WriteHeader(401)
-		//	log.Println(err)
-		//	return
-		//}
-		//defer rows.Close()
-		//defer pkg.Db.Close()
-		//
-		resp, err := json.Marshal(rows)
-		if err != nil {
-			w.WriteHeader(400)
+			resp, err := json.Marshal(rows)
+			if err != nil {
+				w.WriteHeader(400)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(resp)
 			return
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(resp)
-		return
+		if genre != "" {
+			rows, err := pkg.Db.Query(searchResultByGenre, genre)
+
+			for rows.Next() {
+				var name string
+				err := rows.Scan(&name)
+
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				fmt.Println(name)
+			}
+			defer rows.Close()
+
+			restaurantByGenre, err := json.Marshal(rows)
+			if err != nil {
+				w.WriteHeader(400)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(restaurantByGenre)
+			return
+		}
+
+		if rating != "" {
+			rating, err := strconv.Atoi(rating)
+			if err != nil {
+				log.Println(err)
+			}
+
+			rows, err := pkg.Db.Query(searchResultByRating, rating)
+
+			for rows.Next() {
+				var name string
+				err := rows.Scan(&name)
+
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				fmt.Println(name)
+			}
+			defer rows.Close()
+
+			restaurantByRating, err := json.Marshal(rows)
+			if err != nil {
+				w.WriteHeader(400)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(restaurantByRating)
+			return
+
+		}
 	}
+	defer pkg.Db.Close()
+
 	return
 }
